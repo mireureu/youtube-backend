69d1e7e19d960ecec52fe109d86dac94ce63a711
mireureu <j96410@naver.com>
2023-09-08 15:27:32 +0900

    20230908-2

--- src/main/java/com/kh/youtube/domain/Channel.java
+++ src/main/java/com/kh/youtube/domain/Channel.java
@@ -1,26 +1,37 @@
 package com.kh.youtube.domain;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 import java.util.Date;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
 public class Channel {
+
+	@Id
+	@Column(name="channel_code")
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "channelSequence")
+	@SequenceGenerator(name="channelSequence", sequenceName = "SEQ_CHANNEL", allocationSize = 1)
 	private int channelCode;
+
+	@Column(name="channel_name")
 	private String channelName;
+	@Column(name="channel_photo")
 	private String channelPhoto;
+	@Column(name="channel_desc")
 	private String channelDesc;
+	@Column(name="channel_date")
 	private Date channelDate;
-	
-	//@ManyToOne // Channel 엔티티와 Member 엔티티를 다대일 관계로 설정
-	//다대일 관계 (한 멤버가 여러 채널을 가지고있다)
-	//@JoinColumn(name="member_id") //외래키 생성 or Member 엔티티의 기본키와 매핑
+
+	@ManyToOne // Channel 엔티티와 Member 엔티티를 다대일 관계로 설정
+	@JoinColumn(name="id") // 외래키 생성
 	private Member member;
 
 	
